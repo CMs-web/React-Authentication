ce68cbfb6ed733b852bad7a78716ca790b8c8014
Shivam Goswami <shivamgoswami.work@gmail.com>
2024-07-20 14:27:00 +0530

    Add Controllers

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,40 @@
+const User = require("../model/userModel")
+const bcrypt = require("bcrypt")
+
+const register = async(req, res) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        return res.status(400).json({msg : "please fill all fields"})
+    }
+
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(password,salt)
+
+    const user = await User.create({
+        name,
+        email,
+        password : hashedPassword
+    })
+
+    return res.status(200).json(user).select("-password")
+}
+
+
+const login = async(req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({msg : "please fill all fields"})
+    }
+
+    const user = await User.find({ email })
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      return res.status(200).json(user).select("-password");
+    } else {
+      return res.status(404).json({
+        msg: "Try to Register First",
+      });
+    }
+}
+
+module.exports = {register, login}
