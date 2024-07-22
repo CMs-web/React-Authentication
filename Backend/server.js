11215cda3573eea5bfc03439c4c9c9c500189560
Shivam Goswami <shivamgoswami.work@gmail.com>
2024-07-22 16:51:28 +0530

    add cors

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDb = require("./config/db_config");
 const app = express();
+const cors = require("cors")
 
 require("dotenv").config();
 
@@ -9,6 +10,7 @@ app.use(express.urlencoded({extended : true}))
 
 // connetDataBase
 connectDb()
+app.use(cors())
 
 app.get("/", (req, res) => res.send("WELCOME TO AUTH"));
 
